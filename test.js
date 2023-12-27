764d277f9b15eb6e0486eff5ce2bd9704874fc59
«AnastasiiaPerova» <anastation120398@gmaail.com>
2023-12-27 14:51:05 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,10 @@
+// const firstName = 'Anastasiia'
+// firstName = 'Nastya'
+// console.log('Hello', firstName)
+
+let firstName = 'Nastya'
+firstName = 'Anastasiia'
+let age = '25'
+let city = 'Edmonton'
+let hobby = 'skiing'
+console.log('Hello,', firstName, age, city, hobby)
