7e157894e32261aeab8d7e3517620a570120c3e7
«AnastasiiaPerova» <anastation120398@gmaail.com>
2023-12-28 11:03:57 +0300

    lesson2.3

---
+++ lesson2.3/2_3.js
@@ -0,0 +1,8 @@
+const titleProject = "Оценка стоимости";
+const screensValue = "шаблонные, с уникальным дизайном, с анимациями";
+const screenPrice = 1800;
+const percentage = 30;
+const fullPrice = 1000000;
+const responsive = true;
+console.log(typeof titleProject, typeof screensValue, typeof screenPrice,
+    typeof percentage, typeof fullPrice, typeof responsive);
